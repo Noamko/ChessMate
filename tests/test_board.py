0ef59df0aa7d521d6d5e486ac7a33bd972fc0928
noamk <noamko25@Gmail.com>
2023-06-18 22:52:04 +0100

    initial led tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -2,37 +2,44 @@
 import unittest
 import sys
 import os
-sys.path.append(f"{os.getcwd()}/Chessm8/Board")
+sys.path.append(f"../Chessm8/Board")
 import time
 import Board
 from Board import Commands
-
+import serial.tools.list_ports
 class TestBoard(unittest.TestCase):
     def setUp(self):
         # print(f"{os.getcwd()}/Board")
-        self.board_com = Board.SerialCommunication("/dev/tty.usbserial-10", 115200)
+        self.board_com = None
+        for port in serial.tools.list_ports.comports():
+            print(f"port:::: {port.device}, {port.name}, {port.description}, {port.hwid}, {port.vid}, {port.pid}")
+            if port.pid != None:
+                self.board_com = Board.SerialCommunication(port.device, 115200)
+                break
+            if self.board_com is None:
+                raise Exception("No board found")
     
     def test_board_communication(self):
         print("Seding ping")
         # create a ping message
         command = Commands.PING_REQUEST
-        args = []
-        message = Board.BoardMessage.create(command=command, args=args)
+        args = [0]
+        message = Board.BoardRequest.create(command=command, args=args)
         self.board_com.send(message)
 
         print("Waiting for response")
-        response = int.from_bytes(self.board_com.read(1))
+        response = int.from_bytes(self.board_com.read(1), byteorder='little')
         print(f"Got response: {response}")
         self.assertEqual(response, Commands.PING_RESPONSE)
 
     def test_get_board_state(self):
         command = Commands.GET_BOARD_STATE_REQUEST
-        args = []
-        message = Board.BoardMessage.create(command=command, args=args)
+        args = [0]
+        message = Board.BoardRequest.create(command=command, args=args)
         self.board_com.send(message)
 
         print("Waiting for response")
-        id = int.from_bytes(self.board_com.read(1))
+        id = int.from_bytes(self.board_com.read(1), byteorder='little')
         print(f"Got id: {id}")
         len_data = self.board_com.read(4)
         args_len = int.from_bytes(len_data, byteorder='little')
@@ -43,9 +50,9 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(args_len, 1)
     
     def test_leds(self):
-        command = Board.commands.SET_LEDS_STATE_REQUEST
-        args = [0xff]
-        message = Board.BoardMessage.create(command=command, args=args)
+        command = Board.Commands.SET_LEDS_STATE_REQUEST
+        args = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+        message = Board.BoardRequest.create(command=command, args=args)
         self.board_com.send(message)
 
         print("Waiting for response")
